1b19821f1b830c7f82d258099f917647ae3a9cf8
Quang Tran <tquang1708@gmail.com>
2021-10-15 05:06:49 -0400

    update option

--- src/options.js
+++ src/options.js
@@ -1,6 +1,6 @@
 export const options = {
     "client_id": "8e9896a45be540c3b7260c13d4abe33d",
-    "redirect_uri": "http://localhost:3000/callback",
+    "redirect_uri": "https://tquang1708.github.io/spotify-custom-radio/callback",
     "playlist_description": "Your custom radio station, created from <insert url here>",
     "enable_rate_limit": true,
     "entry_count_limit": 50,
