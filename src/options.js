a5e57edd877e729ffb0489aedbc5bb95a679422f
Quang Tran <tquang1708@gmail.com>
2021-10-15 17:37:17 -0400

    redo options js (there is probably a better way)

--- src/options.js
+++ src/options.js
@@ -1,7 +1,6 @@
 export const options = {
     "client_id": "8e9896a45be540c3b7260c13d4abe33d",
-    // "redirect_uri": "https://tquang1708.github.io/spotify-custom-radio/#/",
-    "redirect_uri": "http://localhost:3000/#/",
+    "redirect_uri": "https://tquang1708.github.io/spotify-custom-radio/#/",
     "playlist_description": "Your custom radio station, created from tquang1708.github.io/spotify-custom-radio/",
     "enable_rate_limit": true,
     "entry_count_limit": 50,
