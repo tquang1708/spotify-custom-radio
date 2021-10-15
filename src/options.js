9840228f4ddfc73412cabe28f6d0c69b7064bd71
Quang Tran <tquang1708@gmail.com>
2021-10-15 04:36:59 -0400

    add options js

---
+++ src/options.js
@@ -0,0 +1,8 @@
+export const options = {
+    "client_id": "8e9896a45be540c3b7260c13d4abe33d",
+    "redirect_uri": "http://localhost:3000/callback",
+    "playlist_description": "Your custom radio station, created from <insert url here>",
+    "enable_rate_limit": true,
+    "entry_count_limit": 50,
+    "album_count_limit": 200,
+}
